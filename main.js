9eba21d627cbdfffa95f881efcf8cae77bc2c15b
Risha2011 <74859480+Risha2011@users.noreply.github.com>
2021-01-26 22:55:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -24,3 +24,34 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
   console.log('Model Loaded!');
 }
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_gesture_name").innerHTML = results[0].label;
+    prediction = results[0].label;
+    speak();
+    if(results[0].label == "peace")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#x262E;";
+    }
+    if(results[0].label == "ok")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128077;";
+    }
+    if(results[0].label == "no")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128078;";
+    }
+    if(results[1].label == "amazing")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128076;";
+    }
+  }
+}
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
